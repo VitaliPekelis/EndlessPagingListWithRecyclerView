febf7f97964c0d5ef37cf00c9f7dfe9e4fc629ee
vitali <pedalka@gmail.com>
2019-08-03 23:50:16 +0300

    minor changes

--- app/src/main/java/com/vit/pek/MainActivity.java
+++ app/src/main/java/com/vit/pek/MainActivity.java
@@ -13,7 +13,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         RecyclerView recyclerView = findViewById(R.id.main_activity_rcv);
-        recyclerView.addItemDecoration(new MarginItemDecoration((int) getResources().getDimension(R.dimen.item_space)));
+
+        int spaceInt = (int) (/*(PX)*/ getResources().getDisplayMetrics().density *
+                /*(DP)*/ getResources().getDimension(R.dimen.item_space));
+
+        recyclerView.addItemDecoration(new MarginItemDecoration(spaceInt));
 
         // TODO: Vitali 2019-08-03  to be continue
     }
